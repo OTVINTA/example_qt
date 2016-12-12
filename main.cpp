fb37768b06fabf502b3fb78aaab295732ab40594
Danilin Vsevolod <danilin_vsevolod@mail.ru>
2016-12-12 12:35:55 +0300

    Creat textline

--- main.cpp
+++ main.cpp
@@ -11,9 +11,28 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc,argv);
 
-    QWidget *widget = new QWidget;
+    QPushButton quitButton("Quit");
+    QLineEdit textline1;
+    QLineEdit textline2;
 
-    widget->show();
+
+    QObject::connect(&textline1,SIGNAL(textChanged(QString)),&textline2,SLOT(setText(QString)));
+    QObject::connect(&textline2,SIGNAL(textChanged(QString)),&textline1,SLOT(setText(QString)));
+    QObject::connect(&quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    QVBoxLayout layout;
+    layout.addWidget(&textline1);
+    layout.addWidget(&textline2);
+    layout.addWidget(&quitButton);
+
+    QWidget window;
+    window.setLayout(&layout);
+
+    window.show();
+
+    /*QWidget *widget = new QWidget;
+
+    widget->show();*/
 
     return app.exec();
 }
